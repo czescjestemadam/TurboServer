f556659a513d21860cebe72e9e22b2d32d4789de
nonek <adambimek@gmail.com>
2023-12-05 06:36:55 +0100

    nbt namespace

--- server/utils/nbt/nbt.cc
+++ server/utils/nbt/nbt.cc
@@ -1,1 +1,38 @@
 #include "nbt.hh"
+
+#include "tag/nbt_tag_end.hh"
+#include "tag/nbt_tag_primitive.hh"
+#include "tag/nbt_tag_array.hh"
+#include "tag/nbt_tag_list.hh"
+#include "tag/nbt_tag_compound.hh"
+
+NBT::TagPtr NBT::tagFromType(NbtTagType type)
+{
+	switch (type)
+	{
+		case END: return std::make_unique<NbtTagEnd>();
+		case BYTE: return std::make_unique<NbtTagByte>(0);
+		case SHORT: return std::make_unique<NbtTagShort>(0);
+		case INT: return std::make_unique<NbtTagInt>(0);
+		case LONG: return std::make_unique<NbtTagLong>(0);
+		case FLOAT: return std::make_unique<NbtTagFloat>(0);
+		case DOUBLE: return std::make_unique<NbtTagDouble>(0);
+		case BYTE_ARRAY: return std::make_unique<NbtTagByteArray>();
+		case STRING: return std::make_unique<NbtTagString>("");
+		case LIST: return std::make_unique<NbtTagList>();
+		case COMPOUND: return std::make_unique<NbtTagCompound>();
+		case INT_ARRAY: return std::make_unique<NbtTagIntArray>();
+		case LONG_ARRAY: return std::make_unique<NbtTagLongArray>();
+
+		case UNKNOWN:
+		default:
+			return nullptr;
+	}
+}
+
+NBT::TagPtr NBT::read(std::istream& is)
+{
+
+
+	return ;
+}
--- server/utils/nbt/nbt.hh
+++ server/utils/nbt/nbt.hh
@@ -1,7 +1,17 @@
 #pragma once
+#include "tag/nbt_tag.hh"
+
 #include <string>
 
-class NBT
+namespace NBT
 {
+	using TagPtr = std::unique_ptr<NbtTag>;
 
-};
+	TagPtr tagFromType(NbtTagType type);
+
+
+	TagPtr read(std::istream& is);
+
+	std::string write(TagPtr tag);
+	std::ostream& operator<<(std::ostream& os, TagPtr tag);
+}
