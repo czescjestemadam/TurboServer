d02f028de906d8c9ce938c58b885c686f40f0d7f
nonek <adambimek@gmail.com>
2023-12-15 04:25:03 +0100

    fix testing

--- main.cc
+++ main.cc
@@ -3,37 +3,34 @@
 
 int main(int argc, char* argv[])
 {
-	UUID id = UUID::fromString("OfflinePlayer:_no_onek_");
-	std::cout << id.toString();
+	RunArgs args(argc, argv);
+	args.parse();
 
-//	RunArgs args(argc, argv);
-//	args.parse();
-//
-//	if (args.hasFlag("help"))
-//	{
-//		std::cout << "TurboServer v" + TurboServer::getVersion() << "\n\n"
-//				  << "Usage: TurboServer [options]\n\n"
-//				  << "Options:\n"
-//				  << " --help                 shows help\n"
-//				  << " --version --ver        shows TurboServer version\n"
-//				  << " --default-configs      skips loading/saving configs, using default\n"
-//				  << " --no-logs              skips logging to file\n";
-//
-//		return 0;
-//	}
-//	else if (args.hasFlag("ver") || args.hasFlag("version"))
-//	{
-//		std::cout << "TurboServer v" + TurboServer::getVersion() << '\n';
-//		return 0;
-//	}
-//
-//	ConsoleHandler::init();
-//
-//	Logger logger("TurboServer");
-//	logger.info("Parsed " + args.toString());
-//
-//	TurboServer server(std::move(logger), std::move(args));
-//	server.start();
+	if (args.hasFlag("help"))
+	{
+		std::cout << "TurboServer v" + TurboServer::getVersion() << "\n\n"
+				  << "Usage: TurboServer [options]\n\n"
+				  << "Options:\n"
+				  << " --help                 shows help\n"
+				  << " --version --ver        shows TurboServer version\n"
+				  << " --default-configs      skips loading/saving configs, using default\n"
+				  << " --no-logs              skips logging to file\n";
+
+		return 0;
+	}
+	else if (args.hasFlag("ver") || args.hasFlag("version"))
+	{
+		std::cout << "TurboServer v" + TurboServer::getVersion() << '\n';
+		return 0;
+	}
+
+	ConsoleHandler::init();
+
+	Logger logger("TurboServer");
+	logger.info("Parsed " + args.toString());
+
+	TurboServer server(std::move(logger), std::move(args));
+	server.start();
 
 	return 0;
 }
