c0739b5e3e9591aa031d4d71446bd9c3fa40309a
nonek <adambimek@gmail.com>
2023-12-21 02:37:59 +0100

    WorldType enum

--- server/world/info/world_type.cc
+++ server/world/info/world_type.cc
@@ -1,1 +1,10 @@
 #include "world_type.hh"
+
+WorldType::WorldType(int id, std::string name) : id(id), name(std::move(name))
+{
+}
+
+
+WorldType WorldType::OVERWORLD = { 0, "overworld" };
+WorldType WorldType::NETHER = { 1, "nether" };
+WorldType WorldType::THE_END = { 2, "the_end" };
--- server/world/info/world_type.hh
+++ server/world/info/world_type.hh
@@ -1,7 +1,19 @@
 #pragma once
 
+#include <string>
 
 class WorldType
 {
+	int id;
 
+public:
+	std::string name;
+
+
+	WorldType(int id, std::string name);
+
+
+	static WorldType OVERWORLD;
+	static WorldType NETHER;
+	static WorldType THE_END;
 };
