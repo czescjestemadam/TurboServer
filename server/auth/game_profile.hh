b5cfc86af0d804b7651b238085d551b5ca258797
nonek <adambimek@gmail.com>
2023-12-23 09:04:59 +0100

    mojang api skeleton

--- server/auth/game_profile.hh
+++ server/auth/game_profile.hh
@@ -9,6 +9,8 @@ public:
 	std::string name;
 	UUID uuid;
 
+	std::string textures;
+
 	GameProfile() = default;
 	GameProfile(const std::string& name, const UUID& uuid);
 
--- server/auth/mojang/mojang_api.hh
+++ server/auth/mojang/mojang_api.hh
@@ -3,15 +3,10 @@
 
 #include <string>
 #include <optional>
-#include <unordered_map>
-#include <chrono>
 #include <future>
 
 class MojangAPI
 {
-	static std::unordered_map<std::string, std::pair<UUID, std::chrono::system_clock::time_point>> nameToUuidCache;
-	static std::unordered_map<UUID, std::pair<GameProfile, std::chrono::system_clock::time_point>> uuidToProfileCache;
-
 public:
 	static std::optional<UUID> getUUID(const std::string& name);
 	static std::future<std::optional<UUID>> asyncGetUUID(const std::string& name);
